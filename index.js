a30314cd8b9767c41f19cc41e6b00ac4532c39f1
Mohd Arshad <mohdarshad8668@gmail.com>
2023-04-14 15:27:54 +0530

    added origin for socket.io

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://aquamarine-conkies-edb381.netlify.app",
     // credentials: true,
   },
 });
